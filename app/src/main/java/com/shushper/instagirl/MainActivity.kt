ce12f5ae3495688b29255c81da18894613c6e49e
rfshakhmatov <rfshakhmatov@avito.ru>
2017-07-29 16:34:25 +0300

    loader and errors handling

--- app/src/main/java/com/shushper/instagirl/MainActivity.kt
+++ app/src/main/java/com/shushper/instagirl/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -13,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var disposable: Disposable
 
     private val recycler: RecyclerView by bind(R.id.girls_recycler)
+    private val progress: ViewGroup by bind(R.id.progress)
+
     private val adapter: GirlsAdapter = GirlsAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,18 +32,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadGirls() {
+        showLoad()
+
         val api = (application as App).api
 
         disposable = api.girls()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        { onGirlsLoaded(it) }
+                        { onGirlsLoaded(it) },
+                        { onError(it) }
                 )
     }
 
+    private fun onError(it: Throwable?) {
+        hideLoad()
+        Toast.makeText(this, it?.message ?: "unknown error", Toast.LENGTH_SHORT).show()
+    }
+
     private fun onGirlsLoaded(it: List<Girl>) {
+        hideLoad()
         adapter.setGirls(it)
+    }
+
+    private fun showLoad() {
+        progress.visibility = View.VISIBLE
+    }
+
+    private fun hideLoad() {
+        progress.visibility = View.GONE
     }
 
 }
